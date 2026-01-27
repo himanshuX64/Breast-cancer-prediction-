b934a4b582faa08c29d154e13144168e21a6d47b
Himanshu Kumar <160305210+himanshuX64@users.noreply.github.com>
2026-01-27 14:17:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,27 @@
 import streamlit as st
+#import libraries
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+# visualization libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from sklearn.model_selection import train_test_split
+
+# data modeling libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# data perprocessing libraries
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_curve, auc, roc_auc_score,f1_score,confusion_matrix,classification_report
 
 st.title('Breast Cancer Prediction App')
+#loading dataset
+lbc=load_breast_cancer()
+
+df=pd.DataFrame(lbc.data ,columns=lbc.features_name)
+df
 
 st.write('Hello world!')
