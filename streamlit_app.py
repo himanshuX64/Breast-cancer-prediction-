f15b56d468aea7bcb8002efb0a6c2d7801152e8b
Himanshu Kumar <160305210+himanshuX64@users.noreply.github.com>
2026-01-27 14:42:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from sklearn.datasets import load_breast_cancer
 # visualization libraries
 
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import LabelEncoder
 # data modeling libraries
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
@@ -20,7 +20,17 @@ st.title('Breast Cancer Prediction App')
 df=pd.read_csv("https://raw.githubusercontent.com/himanshuX64/CampusX/refs/heads/main/dataset/breast-cancer.csv")
 X=df.drop('diagnosis',axis=1)
 y=df['diagnosis']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+encoder=LabelEncoder()
+X_train=encoder.fit_transform(X_train)
+X_test=encoder.transform(X_test)
 
 with st.expander('Data'):
+  df
+
+with st.expander("Depandent Data ")
   X
+
+with st.expander("Target Data")
+  y
 
