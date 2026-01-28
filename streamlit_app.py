31e0341f58d8717fee7e93d1248463e35de0158f
Himanshu Kumar <160305210+himanshuX64@users.noreply.github.com>
2026-01-28 10:35:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,15 @@ X=df.drop('diagnosis',axis=1)
 y=df['diagnosis']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 encoder=LabelEncoder()
-X_train=encoder.fit_transform(X_train)
-X_test=encoder.transform(X_test)
+X_train_en=encoder.fit_transform(X_train)
+X_test_en=encoder.transform(X_test)
 
 with st.expander('Data'):
   df
 
-with st.expander("Depandent Data "):
-  X_train
+# with st.expander("Depandent Data "):
+#   X_train
 
-with st.expander("Target Data"):
-  y_train
+# with st.expander("Target Data"):
+#   y_train
 
